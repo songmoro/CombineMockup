b238be6d0b48f643312797b1071fd29f812f338c
songmoro <wlqgkr@naver.com>
2023-10-30 16:38:29 +0900

    Initial Commit

---
+++ CombineMockup/CombineMockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineMockupApp.swift
+//  CombineMockup
+//
+//  Created by 송재훈 on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineMockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineMockup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineMockup
+//
+//  Created by 송재훈 on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
